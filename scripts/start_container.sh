c910c674fc24ad0d18311d2b6c123f3a070750eb
vipinyadav23 <143183517+vipinyadav23@users.noreply.github.com>
2024-09-21 19:54:56 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull vipinyadav23/python-test-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 vipinyadav23/python-test-app
